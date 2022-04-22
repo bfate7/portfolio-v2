93564266416b326018f5f0beca9455ca7f8ac362
bfate7 <fateh.developer@gmail.com>
2022-04-22 13:06:05 +0100

    update Hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,25 +1,41 @@
 import React from "react";
 import contact from "../data/contact";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
     <div>
-      <h1 className='text-4xl lg:text-7xl font-bold  text-gray-900 dark:text-gray-300'>
+      <motion.h1
+        className='text-4xl lg:text-7xl font-bold  text-primary-lighten-1 dark:text-primary-lighten-3'
+        animate={{ y: [100, 0] }}
+      >
         Hi, I am Fateh.
-      </h1>
+      </motion.h1>
 
-      <h1 className='text-2xl lg:text-5xl font-bold text-gray-600 dark:text-gray-400 mt-5'>
+      <motion.h1
+        className='text-2xl lg:text-5xl font-bold text-primary-lighten-2 dark:text-primary-lighten-2 mt-5'
+        animate={{ y: [100, 0] }}
+        transition={{ delay: 0.05 }}
+      >
         I build things for the web.
-      </h1>
-      <h2 className='text-xl text-gray-500 mt-5'>
+      </motion.h1>
+      <motion.h2
+        className='text-xl text-gray-500 mt-5'
+        animate={{ opacity: [0, 1], y: [50, 0] }}
+        transition={{ delay: 0.3 }}
+      >
         I &apos; m Full Stack Web Developer.
-      </h2>
+      </motion.h2>
 
-      <a href={`mailto:${contact.email}`}>
-        <button className='mt-10 border border-green-400 rounded-sm px-4 py-2 hover:bg-green-400 hover:text-gray-100 text-green-400'>
+      <motion.a
+        href={`mailto:${contact.email}`}
+        animate={{ opacity: [0, 1] }}
+        transition={{ delay: 1.5 }}
+      >
+        <button className='mt-10 border border-secondary rounded-sm px-4 py-2 hover:bg-secondary hover:text-white text-secondary'>
           Get In Touch
         </button>
-      </a>
+      </motion.a>
     </div>
   );
 };
