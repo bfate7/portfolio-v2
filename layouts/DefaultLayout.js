8f303e1e342082f7f13cd9307231504c21de7c28
bfate7 <fateh.developer@gmail.com>
2022-04-22 13:31:43 +0100

    update DefaultLayout

--- layouts/DefaultLayout.js
+++ layouts/DefaultLayout.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import Head from "next/head";
 import NavBar from "../components/NavBar";
 import SideLinks from "../components/SideLinks";
 import { ThemeContext } from "../context/ThemeContext";
@@ -6,16 +7,23 @@ import { ThemeContext } from "../context/ThemeContext";
 const DefaultLayout = ({ children }) => {
   const { darkTheme } = useContext(ThemeContext);
   return (
-    <div
-      className={`min-h-screen  ${darkTheme ? "dark bg-primary" : "bg-light"}`}
-    >
-      {" "}
-      <NavBar />
-      <SideLinks />
-      <div className='w-10/12 lg:w-8/12 xl:w-6/10 mx-auto container'>
-        {children}
+    <>
+      <Head>
+        <title>B.Fate7</title>
+      </Head>
+      <div
+        className={`min-h-screen  ${
+          darkTheme ? "dark bg-primary" : "bg-light"
+        }`}
+      >
+        {" "}
+        <NavBar />
+        <SideLinks />
+        <div className='w-10/12 lg:w-8/12 xl:w-6/10 mx-auto container'>
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
