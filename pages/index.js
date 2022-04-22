b758f157f9177921f0cc0a565145e037adb986c2
bfate7 <fateh.developer@gmail.com>
2022-04-22 13:13:38 +0100

    update index page

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Skills from "../components/Skills";
-import Experience from "../components/Experience";
 
 export default function Home() {
   return (
@@ -17,7 +16,7 @@ export default function Home() {
         <About />
       </div>
 
-      <div className='pt-20' id='skills'>
+      <div className='py-40' id='skills'>
         <Skills />
       </div>
 
